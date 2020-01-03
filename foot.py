10c1a9adb4892810ff925744ad2d4549d10df930
Valmnt <valentin.mont@ynov.com>
2020-01-03 09:26:06 +0100

    v1.0.2

--- foot.py
+++ foot.py
@@ -4,25 +4,21 @@ import webbrowser
 
 index = open('index.html', "w")
 
+link = ["http://channelstream.net/rmcsport_1.php", "http://channelstream.net/rmcsport_2.php",  "http://channelstream.net/rmcsport_3.php"]
+
 rmc1 = "http://channelstream.net/rmcsport_1.php"
 rmc2 = "http://channelstream.net/rmcsport_2.php"
 rmc3 = "http://channelstream.net/rmcsport_3.php"
 
-r1 = requests.get(rmc1)
-r2 = requests.get(rmc2)
-r3 = requests.get(rmc3)
-
-soup1 =  bs4.BeautifulSoup(r1.text, 'html.parser')
-soup2 =  bs4.BeautifulSoup(r2.text, 'html.parser')
-soup3 =  bs4.BeautifulSoup(r3.text, 'html.parser')
-
-frame_box1 = soup1.find_all("iframe", {"id":"video"})
-frame_box2 = soup2.find_all("iframe", {"id":"video"})
-frame_box3 = soup3.find_all("iframe", {"id":"video"})
-
-text_box1 = soup1.find_all("a", {"class":"article_titre"})
-text_box2 = soup2.find_all("a", {"class":"article_titre"})
-text_box3 = soup3.find_all("a", {"class":"article_titre"})
+index.write("<div class='contener'>")
+for url in link:
+    chanel = requests.get(url)
+    soup = bs4.BeautifulSoup(chanel.text, 'html.parser')
+    frame_box = soup.find_all("iframe", {"id":"video"})
+    text_box = soup.find_all("a", {"class":"article_titre"})
+    index.write("<p>" + str(text_box) + "</p>")
+    index.write(str(frame_box))
+index.write("</div>")
 
 index.write("<!DOCTYPE html>"
 + "<html lang='en'>"
@@ -37,17 +33,8 @@ index.write("<!DOCTYPE html>"
 
 "</body>"+
 "</html>")
-
-index.write("<div class='contener'>")
-index.write("<p>"+ str(text_box1) + "</p>")
-index.write(str(frame_box1))
-index.write("<p>"+ str(text_box2) + "</p>")
-index.write(str(frame_box2))
-index.write("<p>"+ str(text_box3) + "</p>")
-index.write(str(frame_box3))
-index.write("</div>")
+print("Script Successeful [👍]")
 index.close()
-
 
 
 
