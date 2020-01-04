03c6c5b5b704670a54c6eb4cef3824eb959ecfa5
Valmnt <valentin.mont@ynov.com>
2020-01-04 13:24:30 +0100

    v1.0.1

--- scripts.py
+++ scripts.py
@@ -1,5 +1,35 @@
 import subprocess
+import webbrowser
+import cgi
+import sys
+import hashlib
+import pyfiglet
 
-subprocess.call("py foot.py")
-subprocess.call("py divertissement.py")
-subprocess.call("py cinema.py")
+ascii_banner = pyfiglet.figlet_format("PySPORT")
+print(ascii_banner)
+
+index = open('index.html', "w")
+
+code = "5c3dba7f46545a7b608fd7ef8954c2cb"
+compteur = 0
+
+form = cgi.FieldStorage()
+keycodes = form.getvalue('keycode')
+keycodes = input("Entrez la clé d'abonnement: ")
+
+if(keycodes == code):
+    print("Script Index.py Successeful [👍]")
+    subprocess.call("py sport.py")
+    subprocess.call("py divertissement.py")
+    subprocess.call("py cinema.py")
+    webbrowser.open_new("index.html")
+    index.close()     
+else:
+    print("ERF....")
+    sys.exit()
+
+print("Script Index.py Successeful [👍]")
+index.close()
+
+
+
--- sport.py
+++ sport.py
@@ -13,7 +13,7 @@ index.write("<!DOCTYPE html>"
     "<meta name='viewport' content='width=device-width, initial-scale=1.0'>"+
     "<meta http-equiv='X-UA-Compatible' content='ie=edge'>" +
     "<link href='style.css' rel='stylesheet'>" +
-    "<title>FCMONt</title>"+
+    "<title>PySPORT</title>"+
 "</head>"+
 "<body>"+
 
